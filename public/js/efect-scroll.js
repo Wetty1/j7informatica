98640ffb81c25131eaf754af896033b049cdb8db
Wesley <wettygamer@gmail.com>
2019-11-12 17:25:42 -0300

    Feito mesclagem com o front(home) e iniciado o sistema de pesquisa

--- public/js/efect-scroll.js
+++ public/js/efect-scroll.js
@@ -2,7 +2,9 @@
 $(window).on('scroll',function(){
     if($(window).scrollTop()){
         $('.navbar').addClass('purple');
+        $('.dropdown-item').addClass('bg-scroll-admin')
     }else{
         $('.navbar').removeClass('purple');
+        $('.dropdown-item').removeClass('bg-scroll-admin')
     }
 })
--- src/controllers/home/index.js
+++ src/controllers/home/index.js
@@ -1,18 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const Produtos = require('./../../models/Produto')
-require('./all')
 
 router.get('/', async (req, res) => {
+    let produtos = await Produtos.find({ativo: true})
+
     if(req.isAuthenticated()) {
-        let produtos = await Produtos.find({ativo: true})
-
-        console.log(produtos)
-
         res.render('main/index', { user: req.user.nome, admin: req.user.nivel, produtos: produtos})
     } else {
-        let produtos = await Produtos.find({ativo: true})
-
         res.render('main/index', {produtos: produtos})
     }
 })
@@ -22,6 +17,21 @@ router.get('/produto/:id', async (req, res) => {
 
     console.log(produto)
     res.render('main/produto', {produto: produto})
+})
+
+router.get('/pesquisa', async (req, res) => {
+    console.log(req.query.search)
+
+    const produtos = await Produtos.find({nome: {$regex: req.query.seach}})
+
+    console.log(produtos)
+    
+    if(req.isAuthenticated()) {
+        res.render('main/index', { user: req.user.nome, admin: req.user.nivel, produtos: produtos})
+    } else {
+        res.render('main/index', {produtos: produtos})
+    }
+    
 })
 
 module.exports = router
